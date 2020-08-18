b126388e4046dbd230e3ccbbf324ee88356b4152
weniver <weniver@gmail.com>
2020-08-18 14:35:40 -0500

    Navigation dependencies yarn i

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
 
 export default function App() {
   return (
