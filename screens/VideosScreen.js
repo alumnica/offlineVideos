47e8b377ec03db93fe8313876e40ccaee9524188
weniver <weniver@gmail.com>
2020-08-28 13:31:17 -0500

    List of names of all contets in storage

--- screens/VideosScreen.js
+++ screens/VideosScreen.js
@@ -1,36 +1,44 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, TouchableOpacity, StyleSheet,Flatlist} from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  FlatList,
+} from "react-native";
 import { colors } from "../helpers/colors.js";
 
-import storage from '@react-native-firebase/storage';
+import storage from "@react-native-firebase/storage";
+import ListItem from "../components/ListItem.js";
 
 const VideosScreen = ({ navigation, route }) => {
-  function listFilesAndDirectories(reference, pageToken) {
-    return reference.list({ pageToken }).then(result => {
-      // Loop over each item
-      result.items.forEach(ref => {
-        console.log(ref.fullPath);
+  const [files, setFiles] = useState([]);
+
+  const _getAllDirectories = async (reference, pageToken) => {
+    try {
+      let data = [];
+      let refs = await reference.list({ pageToken });
+      refs.items.forEach((item, i) => {
+        data.push({ tittle: item.name, key: `${i}` });
       });
+      console.log(data);
+      setFiles(data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-      if (result.nextPageToken) {
-        return listFilesAndDirectories(reference, result.nextPageToken);
-      }
-
-      return Promise.resolve();
-    });
-  }
-
-  const reference = storage().ref('moment_files');
-
-  listFilesAndDirectories(reference).then(() => {
-    console.log('Finished listing');
-  });
-
-
+  useEffect(() => {
+    const reference = storage().ref("moment_files");
+    _getAllDirectories(reference);
+  }, []);
 
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Videos Screen</Text>
+      <View style={styles.listContainer}>
+        <FlatList data={files} renderItem={ListItem} />
+      </View>
     </View>
   );
 };
@@ -39,14 +47,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
     backgroundColor: colors.black,
+    paddingTop: 55,
   },
   text: {
     color: colors.white,
     fontFamily: "DMSerifDisplay_400Regular",
     fontSize: 30,
+    marginBottom: 20,
   },
+  listContainer: { width: "100%" },
 });
 
 export default VideosScreen;
