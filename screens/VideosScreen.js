8a7e316dffd07ff9f3333a0e82cd2d11c003277f
weniver <weniver@gmail.com>
2020-08-28 12:23:28 -0500

    Storage connected ios android

--- screens/VideosScreen.js
+++ screens/VideosScreen.js
@@ -1,8 +1,33 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet,Flatlist} from "react-native";
 import { colors } from "../helpers/colors.js";
 
+import storage from '@react-native-firebase/storage';
+
 const VideosScreen = ({ navigation, route }) => {
+  function listFilesAndDirectories(reference, pageToken) {
+    return reference.list({ pageToken }).then(result => {
+      // Loop over each item
+      result.items.forEach(ref => {
+        console.log(ref.fullPath);
+      });
+
+      if (result.nextPageToken) {
+        return listFilesAndDirectories(reference, result.nextPageToken);
+      }
+
+      return Promise.resolve();
+    });
+  }
+
+  const reference = storage().ref('moment_files');
+
+  listFilesAndDirectories(reference).then(() => {
+    console.log('Finished listing');
+  });
+
+
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Videos Screen</Text>
